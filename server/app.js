53b23ca973decbb8db5677febda9b07e9b84bdfe
Jimmy Lin <jimmylin0309@gmail.com>
2020-07-21 13:59:47 -0700

    add mongo db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
+
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use(
   '/graphql',
