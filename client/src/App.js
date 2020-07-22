c0652462807dc77000fd3344617274d61dc95b4c
Jimmy Lin <jimmylin0309@gmail.com>
2020-07-21 17:00:56 -0700

    initial booklist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
       <h1>Jimmy's Reading List</h1>
+      <BookList />
     </div>
   );
 }
